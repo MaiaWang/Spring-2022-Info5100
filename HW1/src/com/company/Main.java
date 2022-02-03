6acd6469e922c57aba748c84884796fd686a4a7a
yutingwang <wyting1216@163.com>
2022-02-02 19:31:52 -0800

    hw1

--- HW1/src/com/company/Main.java
+++ HW1/src/com/company/Main.java
@@ -14,6 +14,12 @@ public class Main {
         System.out.println(ifAllUnique("abcdefg"));
         System.out.println(ifAllUnique("aguobhdfqg"));
 
+        System.out.println(leapYear(2019));
+        System.out.println(leapYear(2200));
+
+        grade('A');
+        grade('F');
+
     }
 
     //Question 1: Write a program find if String is Palindrome?
@@ -75,59 +81,88 @@ public class Main {
         }
         return true;
     }
+
     //Question 5:  Write a program to determine whether the year is a leap year or not.
-    //
     //Leap Years are any year that can be evenly divided by 4.  A year that is evenly divisible by 100 is a leap year only if it is also evenly divisible by 400.
-    //
+    public static boolean leapYear(int number){
+        if(number % 4 != 0) return false;
+        else if(number % 100 == 0 && number % 400 != 0){
+            return false;
+        }else return true;
+    }
+
     //Question 6: Write a program to calculate the monthly telephone bills as per the following rule: Minimum $ 200 for up to 100 calls.
     //Plus $ 0.60 per call for next 50 calls.
     //Plus $ 0.50 per call for next 50 calls.
     //Plus $ 0.40 per call for any call beyond 200 calls.
-    //
     //Use Switch Statement in the function.
-    //
+    public static double telephoneBill(int call){
+        switch(call/50){
+            case 0:
+            case 1:
+                return 200;
+            case 2:
+                return (call-100) * 0.6 + 200;
+            case 3:
+                return 200 + 50 * 0.6 + (call - 150)* 0.5;
+            default:
+                return 200 + 50 * 0.6 + 50 * 0.5 + (call - 200)* 0.4;
+        }
+
+    }
+
     //Question 7: Given two strings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
-    //
     //Each letter in magazine can only be used once in ransomNote.
-    //
+    public static boolean ransomNote(String ransomNote, String magazine){
+        Map<Character, Integer> magazineMap = new HashMap<>();
+        Map<Character, Integer> ransomNoteMap = new HashMap<>();
+        for(char c : magazine.toCharArray()){
+            magazineMap.put(c,magazineMap.getOrDefault(c,0)+1);
+        }
+        for(char c : ransomNote.toCharArray()){
+            if(!magazineMap.containsKey(c)) return false;
+            else if(magazineMap.get(c) == 0){
+                magazineMap.remove(c);
+            }else{
+                magazineMap.put(c, magazineMap.get(c) - 1);
+            }
+        }
+        return true;
+    }
+
     //Question 8 : Write a program that prompts the user’s grade. Your program should display the corresponding meaning of grade as per the following table
-    //
-    //Grade
-    //
-    //Meaning
-    //
-    //A
-    //
-    //Excellent
-    //
-    //B
-    //
-    //Good
-    //
-    //C
-    //
-    //Average
-    //
-    //D
-    //
-    //Deficient
-    //
-    //F
-    //
-    //Failing
-    //
-    //
-    //
+    public static void grade (Character c){
+        switch(c){
+            case 'A':
+                System.out.println("Excellent");
+                break;
+            case 'B':
+                System.out.println("Good");
+                break;
+            case 'C':
+                System.out.println("Average");
+                break;
+            case 'D':
+                System.out.println("Deficient");
+                break;
+            case 'F':
+                System.out.println("Falling");
+                break;
+        }
+    }
+
     //Question 9: Write a program to create a book class. The Book class would have following private variables
-    //
-    //String name;
-    //
-    //int bookID;
-    //
-    //String genres;
-    //
-    //double price;
-    //
+//    public class Book{
+//        String name;
+//        int bookID;
+//        String genres;
+//        double price;
+//    }
+//    public Book (String name, int bookID,String genres,double price){
+//        this.
+//
+//    }
+
     //Create a constructor to initialize the values. Create getters and setters for variables.
     //
     //Question 10: Implement a class Box class Box {
